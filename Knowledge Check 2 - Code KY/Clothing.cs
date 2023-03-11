cf0551200bd3a48571920320de692a8ed17d4b37
samanthawalls <samanthawalls22@gmail.com>
2023-03-11 15:32:47 -0500

    Initial Commit - Project Completed

---
+++ Knowledge Check 2 - Code KY/Clothing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Check_2___Code_KY
+{
+    public class Clothing
+    {
+        public string Material { get; set; }
+        public decimal Price { get; set; }
+        public int NumberInStock { get; set; }
+        public Clothing()
+        {
+
+            Material = string.Empty;
+        }
+    }
+}
---
+++ Knowledge Check 2 - Code KY/Shirt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Check_2___Code_KY
+{
+    internal class Shirt : Clothing
+    {
+        public string Size { get; set; }
+        public string SleeveType { get; set; }
+        public bool IsCollared { get; set; }
+        public bool HasButtons { get; set; }
+        public Shirt()
+        {
+            Size = string.Empty;
+            SleeveType = string.Empty;
+        }
+    }
+}
